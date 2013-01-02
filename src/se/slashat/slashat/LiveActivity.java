e35585564ec70eee259c254ed118111224dd157c
Tommie <slask@tommie.nu>
2013-01-02 23:38:09 +0100

    Fragments added

--- src/se/slashat/slashat/LiveActivity.java
+++ src/se/slashat/slashat/LiveActivity.java
@@ -1,27 +1,51 @@
 package se.slashat.slashat;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import android.widget.Toast;
 
-public class LiveActivity extends SherlockActivity {
+public class LiveActivity extends SherlockFragmentActivity implements ActionBar.TabListener {
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    	ActionBar.Tab newTab0 = getSupportActionBar().newTab();
+    	newTab0.setText("Tab 0 title");
+    	ActionBar.Tab newTab1 = getSupportActionBar().newTab();
+    	newTab1.setText("Tab 1 title");
+    	ActionBar.Tab newTab2 = getSupportActionBar().newTab();
+    	newTab2.setText("Tab 2 title");
+    	
+    	newTab0.setTabListener(this);
+    	newTab1.setTabListener(this);
+    	newTab2.setTabListener(this);
+
+    	getSupportActionBar().addTab(newTab0);
+    	getSupportActionBar().addTab(newTab1);
+    	getSupportActionBar().addTab(newTab2);
+    	
+    }
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-        getSupportActionBar().setIcon(R.drawable.ic_launcher);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(true); 
-		
-		setContentView(R.layout.activity_live);
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		com.actionbarsherlock.view.MenuInflater inflater = getSupportMenuInflater();
-		inflater.inflate(R.menu.activity_live, menu);
-		return true;
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		toastText("tab " + String.valueOf(tab.getPosition()) + " clicked");
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	private void toastText(String message){
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 	}
 
 }
