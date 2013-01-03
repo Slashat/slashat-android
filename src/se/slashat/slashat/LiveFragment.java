39b6e59906283490f7b7b294b6a643d2059f96b4
Tommie <slask@tommie.nu>
2013-01-03 20:59:31 +0100

    WebView updates for bambuser (not finished

--- src/se/slashat/slashat/LiveFragment.java
+++ src/se/slashat/slashat/LiveFragment.java
@@ -1,15 +1,21 @@
 package se.slashat.slashat;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class LiveFragment extends Fragment {
 	
 	private static final Boolean ForceLive = true;
+	private static final String TAG = "Slashat-LiveFragment";
 	WebView mWebView;
 	
     @Override
@@ -35,15 +41,52 @@ public class LiveFragment extends Fragment {
 		super.onActivityCreated(savedInstanceState);
 		mWebView = (WebView) getView().findViewById(R.id.webView1);
 		
+		//load our webclient
+        mWebView.setWebViewClient(new SlashatWebViewClient());
+        
+        //settings
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setPluginsEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        
 		//load bambuser
 		//we should probably bounce this via the website, in case we change streaming-partner
 		//eg http://slashat.se/mobile/livestream.php
 		try {
-			mWebView.loadUrl("http://embed.bambuser.com/channel/slashat");
+			mWebView.loadUrl("http://embed.bambuser.com/channel/slashat?autoplay=1&chat=0");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	
 	}
+	
+	/*
+	 * 
+	 * We need this class to override the default webbrowser behavior in Android.
+	 * 
+	 */
+	private class SlashatWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			Log.d(TAG, "Browser Loading: " + url);
+			return true;
+		}
+
+		@Override
+		public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+			Log.d(TAG, "Browser Error: " + errorCode + " - " + description);
+		}
+
+		@Override
+		public void onPageStarted(WebView view, String url, Bitmap favicon) {
+
+		}
+
+		@Override
+		public void onPageFinished(WebView view, String url) {
+
+		}
+	}
+	
 }
