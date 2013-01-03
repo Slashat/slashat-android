51d56821662bb3fad665e8953bd36edee09a1494
Tommie <slask@tommie.nu>
2013-01-03 13:45:28 +0100

    Basic fragments added with support down to 2.1

--- src/se/slashat/slashat/LiveActivity.java
+++ src/se/slashat/slashat/LiveActivity.java
@@ -3,8 +3,10 @@ package se.slashat.slashat;
 import android.os.Bundle;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+
 import android.support.v4.app.FragmentTransaction;
 import com.actionbarsherlock.app.ActionBar.Tab;
+
 import android.widget.Toast;
 
 public class LiveActivity extends SherlockFragmentActivity implements ActionBar.TabListener {
@@ -14,18 +16,18 @@ public class LiveActivity extends SherlockFragmentActivity implements ActionBar.
         
         getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-    	ActionBar.Tab newTab0 = getSupportActionBar().newTab();
-    	newTab0.setText("Tab 0 title");
+    	ActionBar.Tab tabLive= getSupportActionBar().newTab();
+    	tabLive.setText("Live");
     	ActionBar.Tab newTab1 = getSupportActionBar().newTab();
-    	newTab1.setText("Tab 1 title");
+    	newTab1.setText("Arkiv");
     	ActionBar.Tab newTab2 = getSupportActionBar().newTab();
-    	newTab2.setText("Tab 2 title");
+    	newTab2.setText("Om Oss");
     	
-    	newTab0.setTabListener(this);
+    	tabLive.setTabListener(this);
     	newTab1.setTabListener(this);
     	newTab2.setTabListener(this);
 
-    	getSupportActionBar().addTab(newTab0);
+    	getSupportActionBar().addTab(tabLive);
     	getSupportActionBar().addTab(newTab1);
     	getSupportActionBar().addTab(newTab2);
     	
@@ -37,15 +39,21 @@ public class LiveActivity extends SherlockFragmentActivity implements ActionBar.
 
 	@Override
 	public void onTabSelected(Tab tab, FragmentTransaction ft) {
-		toastText("tab " + String.valueOf(tab.getPosition()) + " clicked");
+		this.toastText(tab.getText().toString());
+		
+		LiveFragment frag = new LiveFragment();
+		ft.replace(android.R.id.content, frag);
 	}
 
 	@Override
 	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		
 	}
 
 	private void toastText(String message){
 		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 	}
+	
+	
 
 }
