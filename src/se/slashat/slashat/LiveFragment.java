7a4b94f19cd6c59472798eb59e184601693fa68d
Tommie <slask@tommie.nu>
2013-01-03 15:25:50 +0100

    Added livewindows shard and basic functionality for on or offlive data

--- src/se/slashat/slashat/LiveFragment.java
+++ src/se/slashat/slashat/LiveFragment.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class LiveFragment extends Fragment {
+		
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live, container, false);
+    	return inflater.inflate(R.layout.fragment_live, container, false);
     }
 }
