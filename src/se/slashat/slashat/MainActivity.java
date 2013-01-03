059db41eb72d0a9a2b2c2dd1f47ef3defeb4ca8b
Tommie <slask@tommie.nu>
2013-01-03 13:46:33 +0100

    Refractored code in to MainActivity

--- src/se/slashat/slashat/MainActivity.java
+++ src/se/slashat/slashat/MainActivity.java
@@ -1,24 +1,59 @@
 package se.slashat.slashat;
 
-import com.actionbarsherlock.app.SherlockActivity;
 import android.os.Bundle;
-import android.content.Intent;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class MainActivity extends SherlockActivity {
+import android.support.v4.app.FragmentTransaction;
+import com.actionbarsherlock.app.ActionBar.Tab;
 
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		//decide if we're showing live (tuesdays 19.00 -> 22.00) or Nextshow 
-		
-		//live for now
-        try {
-			Intent intent = new Intent(this, LiveActivity.class);
-			startActivity(intent);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-			
+import android.widget.Toast;
+
+public class MainActivity extends SherlockFragmentActivity implements ActionBar.TabListener {
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+    	ActionBar.Tab tabLive= getSupportActionBar().newTab();
+    	tabLive.setText("Live");
+    	ActionBar.Tab newTab1 = getSupportActionBar().newTab();
+    	newTab1.setText("Arkiv");
+    	ActionBar.Tab newTab2 = getSupportActionBar().newTab();
+    	newTab2.setText("Om Oss");
+    	
+    	tabLive.setTabListener(this);
+    	newTab1.setTabListener(this);
+    	newTab2.setTabListener(this);
+
+    	getSupportActionBar().addTab(tabLive);
+    	getSupportActionBar().addTab(newTab1);
+    	getSupportActionBar().addTab(newTab2);
+    	
+    }
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
 	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		this.toastText(tab.getText().toString());
+		
+		LiveFragment frag = new LiveFragment();
+		ft.replace(android.R.id.content, frag);
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		
+	}
+
+	private void toastText(String message){
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+	}
+	
+	
 
 }
