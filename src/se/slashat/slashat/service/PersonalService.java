0d52a5dd9e1ca5607ebe279db3efaa470c7e5722
Nicklas Löf <nicklaslof76@gmail.com>
2013-01-12 22:20:37 +0100

    Fixes UTF-8 (I hope)

--- src/se/slashat/slashat/service/PersonalService.java
+++ src/se/slashat/slashat/service/PersonalService.java
@@ -11,15 +11,15 @@ public class PersonalService {
 		Personal[] personal = new Personal[4];
 
 		personal[0] = new Personal(
-				"Jezper Söderlund",
+				"Jezper S√∂derlund",
 				R.drawable.jezper,
 				"kontakt@jezper.se",
 				"jezperse",
 				"http://www.jezper.se",
-				"En glad och bekväm herre, årsmodell 1980. Jobbar som originalare men är också aktiv inom dansmusikscenen som producenten och DJ:n Airbase."
-						+ "\n\nHan spenderar orimligt mycket tid framför datorer och har numer helt och hållet lämnat PC-land för en hundraprocentig Apple-miljö."
-						+ "Några Linux-dojor har han aldrig velat ha. Intresset för datorer och teknik har hängt med sedan Amiga 500:an."
-						+ "\n\nÖvriga intressen innefattar musik, film, resor, gott öl och vin, motorcyklar, golf och egentligen allt som hör livsnjutning till.");
+				"En glad och bekv√§m herre, √•rsmodell 1980. Jobbar som originalare men √§r ocks√• aktiv inom dansmusikscenen som producenten och DJ:n Airbase."
+						+ "\n\nHan spenderar orimligt mycket tid framf√∂r datorer och har numer helt och h√•llet l√§mnat PC-land f√∂r en hundraprocentig Apple-milj√∂."
+						+ "N√•gra Linux-dojor har han aldrig velat ha. Intresset f√∂r datorer och teknik har h√§ngt med sedan Amiga 500:an."
+						+ "\n\n√ñvriga intressen innefattar musik, film, resor, gott √∂l och vin, motorcyklar, golf och egentligen allt som h√∂r livsnjutning till.");
 
 		personal[1] = new Personal(
 				"Tommie Podziemski",
@@ -27,10 +27,10 @@ public class PersonalService {
 				"tommie@tommie.nu",
 				"tommienu",
 				"http://www.tommie.nu",
-				"Tommie en tjötig och sportig serie-entreprenör med visst huvud för kod."
-						+ "Vilket allt som oftast innebär att Tommie får en idé på fredagen, spenderar hela helgen på att koda den för att sedan lägga ner allt på måndagen."
-						+ "\n\nKör uteslutande PC (Ubuntu och Windows), och är vad man kallar en Android-fanboy."
-						+ "\n\nHan har en viss förkärlek till att pyssla med linux-kärror och jobbar som IT-ansvarig på ett medelstort svenskt företag och säger sällan, t.om aldrig, nej till en öl.");
+				"Tommie en tj√∂tig och sportig serie-entrepren√∂r med visst huvud f√∂r kod."
+						+ "Vilket allt som oftast inneb√§r att Tommie f√•r en id√© p√• fredagen, spenderar hela helgen p√• att koda den f√∂r att sedan l√§gga ner allt p√• m√•ndagen."
+						+ "\n\nK√∂r uteslutande PC (Ubuntu och Windows), och √§r vad man kallar en Android-fanboy."
+						+ "\n\nHan har en viss f√∂rk√§rlek till att pyssla med linux-k√§rror och jobbar som IT-ansvarig p√• ett medelstort svenskt f√∂retag och s√§ger s√§llan, t.om aldrig, nej till en √∂l.");
 
 		personal[2] = new Personal(
 				"Magnus Jonasson",
@@ -38,19 +38,19 @@ public class PersonalService {
 				"magnus@magnusjonasson.com",
 				"magnusjonasson",
 				"http://www.magnusjonasson.com",
-				"Killen som drar upp medelåldern på Slashat-redaktionen då han är den enda i gänget som är född på 70-talet."
-						+ "\n\nMagnus jobbar till vardags som IT-tekniker och konsult med affärssystem och Windows Server-miljöer som specialitet, men föredrar faktiskt Mac privat."
-						+ "\n\nBeskriver sig själv som en prylnörd och är alltid hungrig på nya prylar att integrera med sitt digitala liv."
-						+ "\n\nFörutom teknik så har en kärlek till matlagning har funnits länge och inte mycket slår hemlagat med en kall öl till. Eller två.");
+				"Killen som drar upp medel√•ldern p√• Slashat-redaktionen d√• han √§r den enda i g√§nget som √§r f√∂dd p√• 70-talet."
+						+ "\n\nMagnus jobbar till vardags som IT-tekniker och konsult med aff√§rssystem och Windows Server-milj√∂er som specialitet, men f√∂redrar faktiskt Mac privat."
+						+ "\n\nBeskriver sig sj√§lv som en pryln√∂rd och √§r alltid hungrig p√• nya prylar att integrera med sitt digitala liv."
+						+ "\n\nF√∂rutom teknik s√• har en k√§rlek till matlagning har funnits l√§nge och inte mycket sl√•r hemlagat med en kall √∂l till. Eller tv√•.");
 		personal[3] = new Personal(
 				"Johan Larsson",
 				R.drawable.johan,
 				"johan@johanl.se",
 				"kottkrig",
 				"http://johanl.se",
-				"Älskar allt som är interaktivt. Utvecklar till vardags multimediaproduktioner och till sängdags så utvecklar jag annat smått och gott."
-						+ "\n\nÄr biOS så jag använder (och utvecklar) lika gärna med Android som iOS. Det finns styrkor och svagheter med allt men jag försöker att använda det som känns mest spännande för stunden."
-						+ "\n\nArbetar hellre i OS X och Linux än i Windows när jag vill vara produktiv. Använder hellre Windows än annat när det kommer till lek & spel.");
+				"√Ñlskar allt som √§r interaktivt. Utvecklar till vardags multimediaproduktioner och till s√§ngdags s√• utvecklar jag annat sm√•tt och gott."
+						+ "\n\n√Ñr biOS s√• jag anv√§nder (och utvecklar) lika g√§rna med Android som iOS. Det finns styrkor och svagheter med allt men jag f√∂rs√∂ker att anv√§nda det som k√§nns mest sp√§nnande f√∂r stunden."
+						+ "\n\nArbetar hellre i OS X och Linux √§n i Windows n√§r jag vill vara produktiv. Anv√§nder hellre Windows √§n annat n√§r det kommer till lek & spel.");
 
 		return personal;
 
