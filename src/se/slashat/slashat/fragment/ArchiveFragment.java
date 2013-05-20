cda81abfff40cf2b5b28a9309c0992856b26fb96
Nicklas Löf <nicklaslof76@gmail.com>
2013-05-20 19:28:39 +0200

    Cache the fragments view and return it. (Faster switching and no crash when activating the archive fragment a second time)

--- src/se/slashat/slashat/fragment/ArchiveFragment.java
+++ src/se/slashat/slashat/fragment/ArchiveFragment.java
@@ -3,6 +3,7 @@ package se.slashat.slashat.fragment;
 import se.slashat.slashat.R;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,23 @@ import android.view.ViewGroup;
  * and then the details are switched on top of it.
  */
 public class ArchiveFragment extends Fragment{
+	
+	private static View view;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (view != null) {
+			ViewGroup parent = (ViewGroup) view.getParent();
+			if (parent != null)
+	            parent.removeView(view);
+		}
+		
+		try{
+			view = inflater.inflate(R.layout.fragment_archive, container, false);
+		} catch (InflateException e) {
+	    }
+		
 		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_archive, container, false);
+		return view;
 	}
 }
