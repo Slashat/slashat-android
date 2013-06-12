af50b723b455c30f6a1431b0e391714008c026be
Nicklas Löf <nicklaslof76@gmail.com>
2013-06-12 19:26:12 +0200

    Card layout in the archives list.

--- src/se/slashat/slashat/adapter/EpisodeAdapter.java
+++ src/se/slashat/slashat/adapter/EpisodeAdapter.java
@@ -4,6 +4,12 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+import org.joda.time.Duration;
+import org.joda.time.Period;
+import org.joda.time.PeriodType;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
+
 import se.slashat.slashat.CallbackPair;
 import se.slashat.slashat.R;
 import se.slashat.slashat.model.Episode;
@@ -23,6 +29,13 @@ public class EpisodeAdapter extends AbstractArrayAdapter<EpisodeViewModel> imple
 	private static final long serialVersionUID = 1L;
 	private CallbackPair<Episode, Boolean> episodeCallback;
 	private SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy", new Locale("sv"));
+	private PeriodFormatter durationFormatter = new PeriodFormatterBuilder()
+    .appendHours()
+    .appendSeparator(":")
+    .appendMinutes()
+    .appendSeparator(":")
+    .appendSeconds()
+    .toFormatter();
 
 	public EpisodeAdapter(Context context, int layoutResourceId, EpisodeViewModel[] data, CallbackPair<Episode, Boolean> episodeCallback) {
 		super(context, layoutResourceId, 0, data);
@@ -35,14 +48,18 @@ public class EpisodeAdapter extends AbstractArrayAdapter<EpisodeViewModel> imple
 	public static class EpisodeHolder extends Holder {
 		TextView episodeNumber;
 		TextView txtTitle;
+		TextView date;
+		TextView length;
 
 	}
 
 	@Override
 	public Holder createHolder(View row) {
 		EpisodeHolder holder = new EpisodeHolder();
-		holder.episodeNumber = (TextView) row.findViewById(R.id.episodeNumberAndTitle);
-		holder.txtTitle = (TextView) row.findViewById(R.id.dateAndLength);
+		holder.episodeNumber = (TextView) row.findViewById(R.id.episodeNumber);
+		holder.txtTitle = (TextView) row.findViewById(R.id.episodeTitle);
+		holder.date = (TextView) row.findViewById(R.id.dateAndLength);
+		holder.length = (TextView) row.findViewById(R.id.length);
 		return holder;
 	}
 
@@ -69,7 +86,34 @@ public class EpisodeAdapter extends AbstractArrayAdapter<EpisodeViewModel> imple
 	@Override
 	public void setDataOnHolder(Holder holder, final EpisodeViewModel episode) {
 		EpisodeHolder eh = (EpisodeHolder) holder;
-		eh.episodeNumber.setText("#" + episode.getModel().getEpisodeNumber() + " - " + episode.getModel().getName());
-		eh.txtTitle.setText(dateFormat.format(episode.getModel().getPublished()) + " - " + episode.getModel().getDuration());
+		eh.episodeNumber.setText("# "+ episode.getModel().getEpisodeNumber());
+		eh.txtTitle.setText(episode.getModel().getName());
+		eh.date.setText(dateFormat.format(episode.getModel().getPublished()));
+		
+		String duration = episode.getModel().getDuration();
+		if (countOccurrences(duration, ':') == 1){
+			duration = "00:"+duration; // For episodes shorter than 1 hour.
+		}
+		
+		
+		Period period = durationFormatter.parsePeriod(duration).normalizedStandard(PeriodType.minutes());
+		String periodString = String.valueOf(period.getMinutes())+"m";
+		eh.length.setText(periodString);
+
+		
 	}
+	
+	public int countOccurrences(String string, char needle)
+	{
+	    int count = 0;
+	    for (int i=0; i < string.length(); i++)
+	    {
+	        if (string.charAt(i) == needle)
+	        {
+	             count++;
+	        }
+	    }
+	    return count;
+	}
+	
 }
