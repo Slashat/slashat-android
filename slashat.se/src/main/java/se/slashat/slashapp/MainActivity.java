074c74c6a74bc751ec4e6e99fca4413688d7f647
Nicklas Löf <nicklaslof76@gmail.com>
2013-06-19 18:10:29 +0200

    Revert "Changed to fully use ActionBarSherlock including new theme extending sherlock light theme. Starts successfully in GB now."
    Reverting since there are problems in Android Studio beta layout designer using themes extended from libraries
    
    This reverts commit 1beed77fdbc67f14ca3520b16b1efe0eff2170b9.

--- slashat.se/src/main/java/se/slashat/slashapp/MainActivity.java
+++ slashat.se/src/main/java/se/slashat/slashapp/MainActivity.java
@@ -2,16 +2,15 @@ package se.slashat.slashapp;
 
 import java.util.Locale;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
-
-import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.*;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -47,7 +46,7 @@ public class MainActivity extends SherlockFragmentActivity implements ActionBar.
         setContentView(R.layout.activity_main);
 
         // Set up the action bar.
-        final ActionBar actionBar = getSupportActionBar();
+        final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         // Create the adapter that will return a fragment for each of the three
