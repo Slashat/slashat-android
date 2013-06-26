fd8bc6fe542f5f29642370f37a2b3418dcdd629c
Nicklas Löf <nicklaslof76@gmail.com>
2013-06-26 07:08:13 +0200

    Bambuser and Google Calendar API keys. This file will be ignored in git after the initial commit and the values need to be replaced with the real values locally.

---
+++ slashat.se/src/main/java/se/slashat/slashapp/Constants.java
@@ -0,0 +1,13 @@
+package se.slashat.slashapp;
+
+/**
+ * Created by nicklas on 6/26/13.
+ */
+public class Constants {
+    // Supply your own Bambuser transcode api key in order to enable the video player
+    public final static String BAMBUSER_TRANSCODE_API_KEY = "";
+    public final static String BAMBUSER_TRANSCODE_URL = "";
+
+    // Google calendar API key
+    public final static String GOOGLE_CALENDAR_API_KEY = "";
+}
