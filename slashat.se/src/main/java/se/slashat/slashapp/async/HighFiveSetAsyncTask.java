ed2ab59a01cc662090f1eeeaf037d9c908930955
Nicklas Löf <nicklaslof76@gmail.com>
2013-10-24 09:27:04 +0200

    Use BasicNameValue pairs instead.  Now it works :-)

--- slashat.se/src/main/java/se/slashat/slashapp/async/HighFiveSetAsyncTask.java
+++ slashat.se/src/main/java/se/slashat/slashapp/async/HighFiveSetAsyncTask.java
@@ -6,11 +6,13 @@ import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpHead;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,6 +20,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import se.slashat.slashapp.Callback;
@@ -42,34 +45,21 @@ public class HighFiveSetAsyncTask extends AsyncTask<String,Void,Boolean> {
         String token = strings[2];
 
         try {
-            JSONObject json = new JSONObject();
-            json.put("receiver", receiver);
-            json.put("token",token);
-
             DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
-
             HttpPost httpPost = new HttpPost(urlString);
 
-            StringEntity stringEntity = new StringEntity(json.toString(),"UTF-8");
-            stringEntity.setContentType("application/json;charset=UTF-8");
-            stringEntity.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE,"application/json;charset=UTF-8"));
+            ArrayList<BasicNameValuePair> postData = new ArrayList<BasicNameValuePair>();
+            postData.add(new BasicNameValuePair("receiver",receiver));
+            postData.add(new BasicNameValuePair("token",token));
 
-            httpPost.setEntity(stringEntity);
-
+            httpPost.setEntity(new UrlEncodedFormEntity(postData));
             HttpResponse response = defaultHttpClient.execute(httpPost);
-
-
-            System.out.println("!!!!!!Sent "+json.toString()+" as "+httpPost.getMethod()+ " to "+urlString + " with contentType");
-            for (HeaderElement headerElement : stringEntity.getContentType().getElements()) {
-                System.out.println(headerElement.getName());
-            }
 
             InputStream stream = response.getEntity().getContent();
             String userJsonString = IOUtils.readStringFromStream(stream);
 
             if (!Strings.isNullOrEmpty(userJsonString)){
                 JSONObject jsonObject = new JSONObject(userJsonString);
-                System.out.println("!!!!!!Got result "+jsonObject.toString()+ " from "+urlString);
                 if (jsonObject.has("success")){
                     if (jsonObject.getBoolean("success")){
                         return true;
