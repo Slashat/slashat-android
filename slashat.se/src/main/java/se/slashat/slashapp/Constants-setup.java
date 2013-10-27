f161ec3dbdd043e0dbfe42f629f9af60bb118af9
tommienu <slask@tommie.nu>
2013-10-27 19:48:15 +0100

    Added setup for Constants

---
+++ slashat.se/src/main/java/se/slashat/slashapp/Constants-setup.java
@@ -0,0 +1,27 @@
+package se.slashat.slashapp;
+
+/**
+ * Created by nicklas on 6/26/13.
+ *
+ * Remove comment-block, add values and rename this file to Constants.Java
+ *
+
+public class Constants {
+    // Supply your own Bambuser transcode api key in order to enable the video player
+    public final static String BAMBUSER_TRANSCODE_API_KEY = "";
+    public final static String BAMBUSER_TRANSCODE_URL = "http://api.bambuser.com/transcode/";
+
+    // Google calendar API key
+    public final static String GOOGLE_CALENDAR_API_KEY = "";
+
+    // Highfive
+
+    public final static String HIGHFIVE_BASE_URL = "";
+    public final static String HIGHFIVE_LOGIN_TO_SERVICE = "";
+    public final static String HIGHFIVE_REGISTER_TO_SERVICE = "";
+    public final static String HIGHFIVE_GET_MY_HIGHFIVES = "";
+    public final static String HIGHFIVE_ALL = "";
+    public final static String HIGHFIVE_DO = "";
+}
+
+ */
